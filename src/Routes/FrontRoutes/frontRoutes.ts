4afb2813a419ee26aa130d210617c71ec78df174
emmaddo <ermsapp@gmail.com>
2024-05-11 09:22:59 +0100

    Stage 5 (Introduced Wildcard Route ): - Introduced wildcard route at the end of routes declaratons in frontRoutes.ts to Send :Hello Wrong Url!: Response to client if undeclared ends on the routes are reached. - I also restructured the json responses.

--- src/Routes/FrontRoutes/frontRoutes.ts
+++ src/Routes/FrontRoutes/frontRoutes.ts
@@ -4,18 +4,34 @@ const front_router = express.Router();
 
 front_router.get("/", (req: Request, res: Response) => {
     // Handle the root path request
-    res.json({message: "Hello Home Page"});
+    //res.json({message: "Hello Home Page"});
+    res.json({ 
+        success: true,
+        data: {
+            message: "Hello Home Page",
+            
+        }
+    });
 });
 
 front_router.get("/About", (req: Request, res: Response) => {
-    // Handle the root path request
-    res.json({message: "Hello About Us Page"});
+    res.json({ 
+        success: true,
+        data: {
+            message: "Hello About Us Page",
+            
+        }
+    });
 });
 
 front_router.post('/', (req: Request, res: Response) => {
-    // Handle POST request for users
+    // Handle POST requests
 });
 
-// More routes related to users...
+
+// Add wildcard route to handle any other URLs
+front_router.all('*', (req: Request, res: Response) => {
+    res.status(404).json({error: "Not Found"});
+});
 
 export default front_router;
